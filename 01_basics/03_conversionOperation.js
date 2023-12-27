d9bdbf79bc90ca64443f978c6f105828516e4094
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2023-12-27 15:44:08 +0000

    conversions of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+// let score = "44"
+// let score = "44fgh"  //NaN
+// let score = null //0
+// let score = undefined;  //NaN
+// let score = true;  //1
+let score = "Abc";  //NaN
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn); 
+
+// 1=> true    0=> false
+// ""=> false
+// "abc"=> true
+
+
+let someNum = 43
+let stringNumber = String(someNum)
+console.log(stringNumber);
+console.log(typeof stringNumber);
