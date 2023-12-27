d545dd4a329d0150688944d5c32ebc52c82e2e63
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2023-12-27 15:26:39 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+// console.log(3
+//     +
+//     3) //code readability should be maintained
+
+console.log("hello rmcs")
+
+let name = "Rmcs"
+let age = 20
+
+// Primitive dataType
+// number => 2 ^ 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value or empty
+let temperature = null
+// undefined => unassigned value
+let state;
+// symbol => for uniqueness
+
+console.log(typeof age); //number
+
+// object
+console.log(typeof null); // object
+
+console.log(typeof state); //undefined
