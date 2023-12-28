6f5a04910912d01f143d4837b3de5b8c052ffbde
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2023-12-28 12:27:23 +0000

    stack and heap updated

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -18,7 +18,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // BigInt
 const bigNumber = 5675658587697696n
@@ -42,16 +42,57 @@ let myFunction = function() {
     console.log("Hello! This is function");
 }
 // Calling
-myFunction()
+// myFunction()
 
 // typeof
-console.log(typeof score);  //number
-console.log(typeof isLoggedIn); //boolean
-console.log(typeof null);   //object =>null
-console.log(typeof userEmail);  //undefined
-console.log(typeof anotherId);  //symbol
-console.log(typeof weekday);  //object =>array
-console.log(typeof obj);    //object =>object
-console.log(typeof myFunction); //function =>known as object function
+// console.log(typeof score);  //number
+// console.log(typeof isLoggedIn); //boolean
+// console.log(typeof null);   //object =>null
+// console.log(typeof userEmail);  //undefined
+// console.log(typeof anotherId);  //symbol
+// console.log(typeof weekday);  //object =>array
+// console.log(typeof obj);    //object =>object
+// console.log(typeof myFunction); //function =>known as object function
 
 // Link - https://tc39.es/ecma262/#sec-typeof-operator
+
+// ------------------------------
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myGitName = "RMCS"
+
+let anotherName = myGitName
+anotherName = "AnotherGitName"
+
+console.log(myGitName);
+console.log(anotherName);
+
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "rmcs@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// stack memory                     // heap memory
+
+// ------------------               --------------------------------
+// |                |               |                              |
+// ------------------               |                              |
+// |    userTwo     |-----          |                              |
+// ------------------     \------>  | {                            |
+// |    userOne     | ------------> |   email : "user@gmail.com",  |
+// ------------------               |   upi : "user@ybl"           |
+// |   myGitName    |               | }                            | 
+// ------------------               |                              |
+// |  anotherName   |               |                              |   
+// ------------------               --------------------------------
+// |   myGitName    |          
+// ------------------       
