cc48faae2671ed266d349cabf8b9b0864cf3227b
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2024-01-09 17:26:02 +0000

    updated scope concepts

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,11 +7,45 @@ if(true){                                  // _
     let a = 10                             //  |
     const b = 20                           //  |-> Block scope
     // c = 30                              //  |
-    console.log(`Inner value of a: ${a}`); // _|
-}                   
+  //console.log(`Inner value of a: ${a}`); //  |
+}                                          // _|
 
 
 
-console.log(`Outer value of a: ${a}`); 
+// console.log(`Outer value of a: ${a}`); 
 // console.log(b); 
 // console.log(c);
+
+function one(){
+    const username = "rm-cs"
+    function two(){
+        const website = "hostinger"
+        // console.log(username);
+    }
+    // console.log(website); //error because website variable is in two function block which can't be access outside function
+    two()
+}
+one()
+
+if(true){
+    const username="rm-cs"
+    if(username === "rm-cs"){
+        const website = "facebook"
+        // console.log(username + " " + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
