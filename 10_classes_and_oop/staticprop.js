824bf6c1b98fb2c608f1f5033acd965fbad14551
Rimjhim Mukherjee <134839601+RM-CS@users.noreply.github.com>
2024-01-30 17:42:07 +0000

    static prop in js

---
+++ 10_classes_and_oop/staticprop.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const rmcs = new User("rmcs")
+// console.log(rmcs.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
